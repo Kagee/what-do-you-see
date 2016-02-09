cc2323d9eec0c29ccdc0e96076da28ad49613391
Anders Einar Hilden <hildenae@gmail.com>
2016-02-09 12:37:47 +0100

    Setup forwarding on mitm/gateway

--- mitm.sh
+++ mitm.sh
@@ -6,21 +6,23 @@ set -e
 # preventing it from even trying to access stdin.
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get --quiet --quiet --yes update
+apt-get -qq --yes update
 
-apt-get --quiet --quiet --yes upgrade
+apt-get -qq --yes upgrade
 
 # Some standard tools
-apt-get install --quiet --quiet --yes language-pack-nb vim
+apt-get install -qq --yes language-pack-nb vim
 
-cd /root
+# Set up iptables before we install iptables-persistent
+iptables --table nat    --add POSTROUTING --out-interface eth0 --jump MASQUERADE
+iptables --table filter --add FORWARD --protocol tcp --match multiport --dports 22,53,80,443,8080,8443 --jump ACCEPT
+iptables --table filter --add FORWARD --protocol udp --match multiport --dports 53                     --jump ACCEPT
+iptables --table filter --add FORWARD --protocol tcp --match multiport --sports 22,53,80,443,8080,8443 --jump ACCEPT
+iptables --table filter --add FORWARD --protocol udp --match multiport --sports 53                     --jump ACCEPT
+iptables --table filter --policy-P FORWARD DROP
 
-echo $HOSTNAME
+apt-get install --yes iptables-persistent
 
-# to avoid unnessesary traffic
-#if [ -d "/vagrant/repo" ]; then
-#  cp -R /vagrant/repo .
-#else
-#  git clone https://github.com/user/repo.git
-#fi
+# It's a gateway, we want ip forward
+sysctl -w net.ipv4.ip_forward=1
 
